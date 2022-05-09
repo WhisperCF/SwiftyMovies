8568df22e094b27d4217488f7940f3c4c7f5d936
Chris Fouts <cbfouts@micromat.com>
2022-05-09 15:53:49 -0700

    Initial Commit

---
+++ TestPlayer/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  TestPlayer
+//
+//  Created by Christopher Fouts on 5/9/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
